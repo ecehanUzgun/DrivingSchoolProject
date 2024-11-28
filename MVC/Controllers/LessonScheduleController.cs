a562322b6554e3bfaaeeec3f1acc94c9dcded781
ecehanUzgun <ecehan766@gmail.com>
2024-11-28 12:06:41 +0300

    function resetForm and js code updated

---
+++ MVC/Controllers/LessonScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class LessonScheduleController : Controller
+    {
+        private readonly IStudentService _studentService;
+        private readonly ITeacherService _teacherService;
+        private readonly IScheduleService _scheduleService;
+        public LessonScheduleController(IStudentService studentService, ITeacherService teacherService)
+        {
+            _studentService = studentService;
+            _teacherService = teacherService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
